e9aca413257c13a3a7b3329210133e7c435a749a
kerrimov <tolik.kerimov@gmail.com>
2018-12-02 19:40:53 +0200

    done SimpleChat

---
+++ network/src/chat/network/TCPConnetionListener.java
@@ -0,0 +1,8 @@
+package chat.network;
+
+public interface TCPConnetionListener {
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection, String value);
+    void onDisconnect(TCPConnection tcpConnection);
+    void onException(TCPConnection tcpConnection, Exception e);
+}
---
+++ server/src/chat/server/ChatServer.java
@@ -0,0 +1,60 @@
+package chat.server;
+
+import chat.network.TCPConnection;
+import chat.network.TCPConnetionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnetionListener {
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer(){
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)){
+            while (true){
+                try {
+                    new TCPConnection(this, serverSocket.accept()); // ждать пока кто-нибудь не подключиться
+                }catch (IOException e){
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected: " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+    }
+
+    private void sendToAllConnections(String value){
+        System.out.println(value);
+        for (int i = 0; i < connections.size(); i++){
+            connections.get(i).sendString(value);
+        }
+    }
+}
